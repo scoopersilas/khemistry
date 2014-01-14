59c94b940aa57a028091f55aa33654d425c80a24
Shana Silas <chefshana@me.com>
2014-01-14 13:24:01 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Khemistry::Application.config.secret_key_base = '1c0f67d7b6cdcc11394ccbfbfdea201e36c6d68b89b716de9c3f003148faf0431a568f8f41a68e6e2ef285cbcc2e067aefab03967a314fa6817b79fcee0c0cd7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Khemistry::Application.config.session_store :cookie_store, key: '_khemistry_session'
